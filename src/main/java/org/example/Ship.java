a7dd8f872936be6caea9dc79e801ac40a12817cb
tHeCreator <marcsabalete@gmail.com>
2023-04-08 16:34:13 +0200

    CommitShipV3

--- src/main/java/org/example/Ship.java
+++ src/main/java/org/example/Ship.java
@@ -3,30 +3,32 @@ package org.example;
 import java.awt.*;
 
 public class Ship {
-       private final Punto puntoDePartida = new Punto(0,0);
+       private Punto puntoFinal;
+       private Punto puntoInicial;
        private int size;
        public CardinalPoints orientation;
        private int toques;
        private boolean hundido;
+         public Ship(Punto p1, Punto p2, int size) {
+              this.size = size;
+              if (p1.getX() == p2.getX() && p1.getY() < p2.getY()){
+                this.orientation = CardinalPoints.NORTH;
+              }else if(p1.getX() < p2.getX() && p1.getY() == p2.getY()){
+                this.orientation = CardinalPoints.EAST;
 
-       public Ship( Punto p,int size) {
-           this.size = size;
-           if (p.getX()>puntoDePartida.getX() && p.getY()> puntoDePartida.getY()){
-               this.orientation = CardinalPoints.NORTH;
-           }else if(p.getX()< puntoDePartida.getX() && p.getY()> puntoDePartida.getY()){
-               this.orientation = CardinalPoints.EAST;
+              } else if (p1.getX() == p2.getX() && p1.getY() > p2.getY()){
+                this.orientation = CardinalPoints.SOUTH;
 
-           } else if (p.getX()< puntoDePartida.getX() && p.getY()< puntoDePartida.getY()){
-               this.orientation = CardinalPoints.SOUTH;
+              } else if (p1.getX() > p2.getX() && p1.getY() == p2.getY()){
+                this.orientation = CardinalPoints.WEST;
+              }
+              this.toques = 0;
 
-           } else if (p.getX()> puntoDePartida.getX() && p.getY()< puntoDePartida.getY()){
-               this.orientation = CardinalPoints.WEST;
-           }
-       }
+              this.hundido = false;
 
-    public Punto getP() {
-        return puntoDePartida;
-    }
+         }
+
+
 
     public int getSize() {
         return size;
@@ -63,11 +65,35 @@ public class Ship {
     public void isSunk(){
            if (this.toques == this.size){
                this.hundido = true;
+               System.out.println("Hundido");
            }
     }
-    public void getShot(){
-           this.toques++;
-           isSunk();
+    public void getShot(Point ShotPoint){
+        if (this.orientation == CardinalPoints.NORTH){
+            if (ShotPoint.getX() == this.puntoInicial.getX() && ShotPoint.getY() >= this.puntoInicial.getY() && ShotPoint.getY() <= this.puntoFinal.getY()){
+                this.toques++;
+                System.out.println("Tocado");
+                this.isSunk();
+            }
+        }else if (this.orientation == CardinalPoints.EAST){
+            if (ShotPoint.getY() == this.puntoInicial.getY() && ShotPoint.getX() >= this.puntoInicial.getX() && ShotPoint.getX() <= this.puntoFinal.getX()){
+                this.toques++;
+                System.out.println("Tocado");
+                this.isSunk();
+            }
+        }else if (this.orientation == CardinalPoints.SOUTH){
+            if (ShotPoint.getX() == this.puntoInicial.getX() && ShotPoint.getY() <= this.puntoInicial.getY() && ShotPoint.getY() >= this.puntoFinal.getY()){
+                this.toques++;
+                System.out.println("Tocado");
+                this.isSunk();
+            }
+        }else if (this.orientation == CardinalPoints.WEST){
+            if (ShotPoint.getY() == this.puntoInicial.getY() && ShotPoint.getX() <= this.puntoInicial.getX() && ShotPoint.getX() >= this.puntoFinal.getX()){
+                this.toques++;
+                System.out.println("Tocado");
+                this.isSunk();
+            }
+        }
     }
 
 }
