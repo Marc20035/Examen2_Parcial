02c871c2f2fe5dc5d8780491c122a43a124f8132
tHeCreator <marcsabalete@gmail.com>
2023-04-08 17:15:16 +0200

    CommitUserV1

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class User {
+    private ArrayList<Ship> ships;
+    private boolean isAlive;
+
+    public User(ArrayList<Ship> ships) throws Exception{
+        if(ships.size() > 0 && ships.size() <= 3){
+            this.ships = ships;
+        }else {
+            throw new Exception("El usuario debe tener entre 1 y 3 barcos");
+        }
+        this.isAlive = true;
+    }
+
 }
